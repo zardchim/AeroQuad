930d7db2c91bd88394e0f4b5c776adc5b6ab0de4
zardchim <zardchim@gmail.com>
2014-02-05 19:58:08 +0800

    Workable code with EZ4 MaxSonar Sensor

--- Libraries/AQ_RangeFinder/MaxSonarRangeFinder_EZ4.h
+++ Libraries/AQ_RangeFinder/MaxSonarRangeFinder_EZ4.h
@@ -5,12 +5,26 @@
 
 #include "RangeFinder.h"
 
-#define SPIKE_FILTER_MARGIN 500 // mm ; changes bigger than this need two samples to take effect
+#define SPIKE_FILTER_MARGIN 300 // mm ; changes bigger than this need two samples to take effect
 
 #define RANGER_COUNT ((sizeof(rangeFinders) / sizeof(struct rangeFinder)))
 
-// last reading used for 'spike' filtter
+//The Arduino Wire library uses the 7-bit version of the address, so the code example uses 0x70 instead of the 8‑bit 0xE0
+#define SensorAddress byte(0x70)
+//The Sensor ranging command has a value of 0x51
+#define RangeCommand byte(0x51)
+//These are the two commands that need to be sent in sequence to change the sensor address
+
+//FIR Filter Variable
+float first_previous = 0;
+float second_lastRange = 0;
+
+// last reading used for 'spike' filter
 short lastRange;
+
+void takeRangeReading();
+word requestRange();
+float thirdorder_fir (float , float , float );
 
 void inititalizeRangeFinders() {
 	takeRangeReading();
@@ -18,16 +32,20 @@ void inititalizeRangeFinders() {
 
 void updateRangeFinders() {
 
-  short range = (short)(	requestRange()	*	(10)	);
+  float range = (float) (requestRange()	*	(10)	);
 
-  // Following will accept the sample if it's either withing "spike margin" of last raw reading or previous accepted reading
-  // otherwise it's ignored as noise
+  range = thirdorder_fir(range, lastRange, second_lastRange);
+  rangeFinderRange[ALTITUDE_RANGE_FINDER_INDEX] = (float)range / 1000.0;
   
-  if ((abs(range - lastRange[rangerToRead]) < SPIKE_FILTER_MARGIN)) {
-    rangeFinderRange[rangeFinders[rangerToRead].target] = (float)range / 1000.0;
+  // Following will accept the sample if it's either withing "spike margin" of last raw reading or previous accepted reading
+  // otherwise it's ignored as noise  
+  if ((abs(range - lastRange) < SPIKE_FILTER_MARGIN)) {
+    rangeFinderRange[ALTITUDE_RANGE_FINDER_INDEX] = (float)range / 1000.0;
   }
-  lastRange = range;
-
+  second_lastRange = lastRange;
+  lastRange = range;  
+    
+  takeRangeReading();
 }
 
 //Commands the sensor to take a range reading
@@ -50,6 +68,16 @@ word requestRange(){
         return word(0);                                        //Else nothing was received, return 0 
     }
 }
+
+float thirdorder_fir (float current_value, float first_previous, float second_previous) {
+	float first_coefficient = 0.6;
+	float second_coefficient = 0.3;
+	float third_coefficient = 0.1;
+	
+	return (first_coefficient*current_value + second_coefficient*first_previous + third_coefficient*second_previous);
+}
+	
+
 
 #endif 
 #endif
